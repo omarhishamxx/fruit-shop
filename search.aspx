3da66b3bf5bcbc9f4595039a0a4dba7d82a0bb30
youssefbassem <121457742+ZyoussefbassemSC@users.noreply.github.com>
2023-06-14 02:19:14 +0300

    search

--- search.aspx
+++ search.aspx
@@ -1,7 +1,22 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Masterpage.Master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Masterpage.Master" AutoEventWireup="true" CodeFile="search.aspx.cs" Inherits="search" %>
 
 <asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+
+    
+    <form id="form1" runat="server">
+        <div>
+            <h2>Search Page</h2>
+            <asp:TextBox ID="txtSearch" runat="server" />
+            <asp:Button ID="btnSearch" runat="server" Text="Search" OnClick="btnSearch_Click" />
+            <hr />
+            <asp:Label ID="lblResults" runat="server" />
+        </div>
+    </form>
+
+
+
+
 </asp:Content>
 
--- search.aspx.cs
+++ search.aspx.cs
@@ -1,14 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Configuration;
 
-public partial class _Default : System.Web.UI.Page
+public partial class search : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
+
+    }
+
+    protected void btnSearch_Click(object sender, EventArgs e)
+    {
+        string searchTerm = txtSearch.Text.Trim();
+
+        using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ToString()))
+        {
+            string query = "SELECT * FROM Products WHERE ProductName, ImageUrl,Description , Price LIKE @SearchTerm";
+
+            using (SqlCommand command = new SqlCommand(query, con))
+
+            {
+
+                command.Parameters.AddWithValue("@SearchTerm", "%" + searchTerm + "%");
+                con.Open(); 
+                command
+                    .ExecuteNonQuery();
+
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+
+                lblResults.Text = $"Found {dt.Rows.Count} result(s) for '{searchTerm}'";
+            }
+        }
+
     }
 }
+
+
+
+
