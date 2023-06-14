cab9b3c64e6c9d274e752e6c5f6dc7a7b1ff144a
omarhishamaliassal <omar1905413@miuegypt.edu.eg>
2023-06-14 06:39:53 +0300

    order

---
+++ adminvieworders.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+public partial class adminvieworders : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["isAdmin"] == null || !(bool)Session["isAdmin"])
+        {
+            // Redirect the user to the login page or any other appropriate page
+            Response.Redirect("login.aspx");
+        }
+        if (!IsPostBack)
+        {
+            Bindadmin();
+        }
+    }
+    private void Bindadmin()
+    {     // Retrieve customer data from the database
+        string connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ToString();
+        string query = "SELECT customer_name, product,  address, quantity,total_price FROM order";
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            SqlCommand command = new SqlCommand(query, connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+
+            // Bind the customer data to the GridView control
+            customersGridView.DataSource = dataTable;
+            customersGridView.DataBind();
+        }
+    }
+    protected void backButton_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("adminpanel.aspx");
+    }
+    protected void customersGridView_RowDeleting(object sender, GridViewDeleteEventArgs e)
+    {
+        // Get the ID of the customer to be deleted
+        int customerId = Convert.ToInt32(customersGridView.DataKeys[e.RowIndex].Value);
+
+        // Delete the customer from the database
+        string connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ToString();
+        string query = "DELETE FROM order WHERE order_id = @CustomerId";
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@CustomerId", customerId);
+
+            connection.Open();
+            int rowsAffected = command.ExecuteNonQuery();
+            if (rowsAffected > 0)
+            {
+                // Refresh the GridView to reflect the changes
+                Bindadmin();
+                Label1.Text = "Customer deleted successfully.";
+            }
+            else
+            {
+                Label1.Text = "Failed to delete the customer.";
+            }
+        }
+    }
+}
